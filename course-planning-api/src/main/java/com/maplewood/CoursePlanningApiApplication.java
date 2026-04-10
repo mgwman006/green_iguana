269bcbc59e2188d88db04447f1248ce6a4ae502a
Maneno Abdallah Mgwami <maneno@anylytical.co.za>
2026-04-11 01:05:15 +0200

    initialized a backend application to get started. I tested  health endpoint for actuator and showing app is running

---
+++ course-planning-api/src/main/java/com/maplewood/CoursePlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.maplewood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlanningApiApplication.class, args);
+	}
+
+}
